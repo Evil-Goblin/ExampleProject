8196c5b8e8a9e6bd84f7e0b49b3b0a1d85677006
ForExample <jyh6197@gmail.com>
2023-08-14 13:00:13 +0900

    feat: Project Initialize

---
+++ src/main/java/pg/packet_generator/PacketGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pg.packet_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacketGeneratorApplication.class, args);
+    }
+
+}
