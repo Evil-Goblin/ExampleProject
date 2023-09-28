8bb92661209f448d9873ec3b2472081b3863b806
ForExample <jyh6197@gmail.com>
2023-09-29 00:08:22 +0900

    feat: TestCode CommentService

---
+++ board-example/src/test/java/hello/board/service/CommentServiceTest.java
@@ -0,0 +1,61 @@
+package hello.board.service;
+
+import hello.board.dto.CommentInsertDto;
+import hello.board.dto.CommentReplyInsertDto;
+import hello.board.entity.Comment;
+import hello.board.entity.Post;
+import hello.board.repository.CommentRepository;
+import hello.board.repository.PostRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+class CommentServiceTest {
+
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @DisplayName("Post에 댓글을 작성한다.")
+    void commentToPostTest() {
+        Post post = new Post("title", "content");
+        postRepository.save(post);
+
+        String commentString = "comment";
+        CommentInsertDto comment = new CommentInsertDto(post.getId(), commentString);
+        commentService.saveComment(comment);
+
+        Comment findComment = commentRepository.findAll().get(0);
+        assertThat(findComment.getPost().getId()).isEqualTo(post.getId());
+        assertThat(findComment.getContent()).isEqualTo(commentString);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 postId로 조회시 IllegalArgumentException이 발생한다.")
+    void invalidPostIdTest() {
+        CommentInsertDto content = new CommentInsertDto(100L, "content");
+
+        assertThatThrownBy(() -> commentService.saveComment(content))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 댓글id에 대댓글을 작성시 IllegalArgumentException이 발생한다.")
+    void invalidCommentIdTest() {
+        CommentReplyInsertDto content = new CommentReplyInsertDto(100L, 100L, "content");
+
+        assertThatThrownBy(() -> commentService.saveReplyComment(content))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
