a34d9a5f9162e63d1ca9ac983a53747b165cf20b
ForExample <jyh6197@gmail.com>
2023-10-02 17:08:46 +0900

    feat: Implement to Posts Page

---
+++ board-example/src/main/java/hello/board/controller/PostController.java
@@ -0,0 +1,26 @@
+package hello.board.controller;
+
+import hello.board.dto.PostListDto;
+import hello.board.service.PageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class PostController {
+
+    private final PageService pageService;
+
+    @GetMapping("posts")
+    public String postList(Pageable pageable, Model model) {
+        Page<PostListDto> postListDtos = pageService.postList(pageable);
+
+        model.addAttribute("posts", postListDtos);
+
+        return "postlist";
+    }
+}
