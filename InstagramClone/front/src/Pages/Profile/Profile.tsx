7b75ff98c00efa2d226178f922244598136a2c75
ForExample <jyh6197@gmail.com>
2023-11-17 00:02:30 +0900

    feat: instagram front clone 01

---
+++ InstagramClone/front/src/Pages/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProfileUserDetails from "../../Components/ProfileComponents/ProfileUserDetails";
+import ReqUserPostPart from "../../Components/ProfileComponents/ReqUserPostPart";
+
+const Profile = () => (
+    <div className='px-20'>
+        <div className=''>
+            <ProfileUserDetails/>
+        </div>
+        <div>
+            <ReqUserPostPart/>
+        </div>
+    </div>
+);
+
+export default Profile;
