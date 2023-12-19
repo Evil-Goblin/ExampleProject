79f1901e969a052125ae8cc3d71280f41466f420
ForExample <jyh6197@gmail.com>
2023-12-20 00:14:38 +0900

    feat: Project Initialize
    Sample For OptionGame part 1

---
+++ OptionGameDemo/src/main/java/hello/optiongamedemo/OptionGameDemoApplication.java
@@ -0,0 +1,13 @@
+package hello.optiongamedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionGameDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptionGameDemoApplication.class, args);
+    }
+
+}
---
+++ OptionGameDemo/src/test/java/hello/optiongamedemo/OptionGameDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.optiongamedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionGameDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
