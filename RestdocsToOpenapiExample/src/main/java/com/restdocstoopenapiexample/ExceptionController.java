676f371f5d44e9708608416ca8e9ef359de2068f
ForExample <jyh6197@gmail.com>
2024-12-09 22:30:35 +0900

    fix: Change ControllerAdvice to RestControllerAdvice
    ExceptionController 의 어노테이션이 RestControllerAdvice 가 아닌 ControllerAdvice 로 설정되어있던 문제 수정

--- RestdocsToOpenapiExample/src/main/java/com/restdocstoopenapiexample/ExceptionController.java
+++ RestdocsToOpenapiExample/src/main/java/com/restdocstoopenapiexample/ExceptionController.java
@@ -3,10 +3,10 @@ package com.restdocstoopenapiexample;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(IllegalStateException.class)
